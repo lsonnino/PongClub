0c2c1c2df700c2cda648593fdd16057117abc282
lsonnino <lorenzo@sonnino.com>
2019-07-19 21:41:14 +0200

    Added Pong

---
+++ game.py
@@ -0,0 +1,216 @@
+import pygame
+
+BALL_RADIUS = 10
+BALL_COLOR = (230, 230, 240)
+BALL_INITIAL_SPEED = 20
+BALL_MIN_SPEED = 10
+BALL_MAX_SPEED = 25
+
+PLAYER_SIZE = (20, 100)
+PLAYER_COLOR = BALL_COLOR
+PLAYER_VELOCITY = 40
+
+VELOCITY_PASS_COEF = 0.5
+
+
+class Game(object):
+    def __init__(self, win_size, number_of_ai):
+        self.ball = Ball(win_size)
+        self.player1 = Player(True, win_size)
+        self.player2 = Player(False, win_size)
+        self.number_of_ai = number_of_ai
+        self.win_size = win_size
+        self.score = (0, 0)
+        self.playing = True
+
+    def step(self):
+        self.player1.step()
+        self.player2.step()
+        val = self.ball.update(self.player1, self.player2)
+
+        if val < 0:
+            self.score = (self.score[0], self.score[1] + 1)
+            self.reset()
+        elif val > 0:
+            self.score = (self.score[0] + 1, self.score[1])
+            self.reset()
+
+        if self.score[0] > 10 or self.score[1] > 10:
+            self.playing = False
+
+    def reset(self):
+        self.ball.reset(self.win_size)
+        self.player1.reset(self.win_size)
+        self.player2.reset(self.win_size)
+
+    def draw(self, window):
+        self.player1.draw(window)
+        self.player2.draw(window)
+        self.ball.draw(window)
+
+
+class Ball(object):
+    def __init__(self, win_size):
+        self.velocity = (BALL_INITIAL_SPEED, 0)
+        self.position = (win_size[0] / 2, win_size[1] / 2)
+        self.win_size = win_size
+
+    def reset(self, win_size):
+        self.velocity = (BALL_INITIAL_SPEED, 0)
+        self.position = (win_size[0] / 2, win_size[1] / 2)
+        self.win_size = win_size
+
+    def pos_to_int(self):
+        return int(self.position[0]), int(self.position[1])
+
+    def draw(self, window):
+        pygame.draw.circle(window, BALL_COLOR, self.pos_to_int(), BALL_RADIUS)
+
+    def update(self, player1, player2):
+        self.check_collision_with_player(player1)
+        self.check_collision_with_player(player2)
+
+        val = 0
+
+        if self.position[0] - BALL_RADIUS < 0:
+            # self.position = (-self.position[0], self.position[1])
+            self.velocity = (-self.velocity[0], self.velocity[1])
+            val = -1
+        elif self.position[0] + BALL_RADIUS >= self.win_size[0]:
+            # self.position = (2 * self.win_size[0] - self.position[0] - BALL_RADIUS, self.position[1])
+            self.velocity = (-self.velocity[0], self.velocity[1])
+            val = 1
+
+        if self.position[1] - BALL_RADIUS < 0:
+            # self.position = (self.position[0], -self.position[1])
+            self.velocity = (self.velocity[0], -self.velocity[1])
+        elif self.position[1] + BALL_RADIUS >= self.win_size[1]:
+            # self.position = (self.position[0], 2 * self.win_size[1] - self.position[1] - BALL_RADIUS)
+            self.velocity = (self.velocity[0], -self.velocity[1])
+
+        self.position = (
+            self.position[0] + self.velocity[0],
+            self.position[1] + self.velocity[1]
+        )
+
+        return val
+
+    def check_collision_with_player(self, player):
+        vel_x, vel_y = self.velocity
+
+        if player.left:
+            if self.position[0] - BALL_RADIUS <= PLAYER_SIZE[0] and \
+                    player.position[1] < self.position[1] < player.position[1] + PLAYER_SIZE[1]:
+                vel_x = abs(vel_x)
+                vel_x += player.velocity[1] * VELOCITY_PASS_COEF
+        else:
+            if self.position[0] + BALL_RADIUS >= self.win_size[0] - PLAYER_SIZE[0] and \
+                    player.position[1] < self.position[1] < player.position[1] + PLAYER_SIZE[1]:
+                vel_x = -abs(vel_x)
+                vel_x -= player.velocity[1] * VELOCITY_PASS_COEF
+
+        if vel_x < 0:
+            if vel_x < -BALL_MIN_SPEED:
+                vel_x = -BALL_MIN_SPEED
+            elif vel_x > -BALL_MAX_SPEED:
+                vel_x = -BALL_MAX_SPEED
+        else:
+            if vel_x < BALL_MIN_SPEED:
+                vel_x = BALL_MIN_SPEED
+            elif vel_x > BALL_MAX_SPEED:
+                vel_x = BALL_MAX_SPEED
+        if vel_y < 0:
+            if vel_y < -BALL_MIN_SPEED:
+                vel_y = -BALL_MIN_SPEED
+            elif vel_y > -BALL_MAX_SPEED:
+                vel_y = -BALL_MAX_SPEED
+        else:
+            if vel_y < BALL_MIN_SPEED:
+                vel_y = BALL_MIN_SPEED
+            elif vel_y > BALL_MAX_SPEED:
+                vel_y = BALL_MAX_SPEED
+
+        self.velocity = (vel_x, vel_y)
+
+
+class Player(object):
+    def __init__(self, left, win_size, ai=False):
+        self.velocity = (0, PLAYER_VELOCITY)
+        self.position = (
+            0 if left else win_size[0] - PLAYER_SIZE[0],
+            (win_size[1] - PLAYER_SIZE[1]) / 2
+        )
+        self.left = left
+        self.ai = ai
+        self.win_size = win_size
+
+    def reset(self, win_size):
+        self.velocity = (0, PLAYER_VELOCITY)
+        self.position = (
+            0 if self.left else win_size[0] - PLAYER_SIZE[0],
+            (win_size[1] - PLAYER_SIZE[1]) / 2
+        )
+        self.win_size = win_size
+
+    def draw(self, window):
+        pygame.draw.rect(window, PLAYER_COLOR, (self.position[0], self.position[1], PLAYER_SIZE[0], PLAYER_SIZE[1]))
+
+    def step(self):
+        if self.ai:
+            pass
+        else:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                self.move(True)
+                self.velocity = (0, -PLAYER_VELOCITY)
+            elif keys[pygame.K_DOWN]:
+                self.move(False)
+                self.velocity = (0, PLAYER_VELOCITY)
+            else:
+                self.velocity = (0, 0)
+
+        self.update()
+
+    def update(self):
+        if self.position[0] < 0:
+            self.position = (
+                0,
+                self.position[1]
+            )
+            self.velocity = (
+                0,
+                self.velocity[1]
+            )
+        elif self.position[0] + PLAYER_SIZE[0] > self.win_size[0]:
+            self.position = (
+                self.win_size[0] - PLAYER_SIZE[0],
+                self.position[1]
+            )
+            self.velocity = (
+                0,
+                self.velocity[1]
+            )
+        if self.position[1] < 0:
+            self.position = (
+                self.position[0],
+                0
+            )
+            self.velocity = (
+                self.velocity[0],
+                0
+            )
+        elif self.position[1] + PLAYER_SIZE[1] > self.win_size[1]:
+            self.position = (
+                self.position[0],
+                self.win_size[1] - PLAYER_SIZE[1],
+            )
+            self.velocity = (
+                self.velocity[0],
+                0
+            )
+
+    def move(self, up):
+        self.position = (
+            self.position[0],
+            self.position[1] + PLAYER_VELOCITY * (-1 if up else 1)
+        )
---
+++ gui.py
@@ -0,0 +1,56 @@
+from game import *
+
+WIN_SIZE = (700, 700)
+FPS = 30
+
+FONT = "Arial"
+FONT_SIZE = 20
+TEXT_COLOR = PLAYER_COLOR
+
+BACKGROUND_COLOR = (77, 77, 77)
+
+
+def game(name, number_of_ai):
+    # Engine materials
+
+    pygame.init()
+    pygame.font.init()
+
+    window = pygame.display.set_mode(WIN_SIZE)
+    pygame.display.set_caption(name)
+
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont(FONT, FONT_SIZE)
+
+    # Game materials
+
+    game = Game(WIN_SIZE, number_of_ai)
+
+    # Running the game
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            # Check special events
+            if event.type == pygame.QUIT:  # Quit
+                running = False
+                playing = False
+                break
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_ESCAPE]:
+            running = False
+            continue
+
+        window.fill(BACKGROUND_COLOR)
+
+        game.step()
+
+        game.draw(window)
+
+        text_surface = font.render(str(game.score[0]) + " - " + str(game.score[1]), False, TEXT_COLOR)
+        window.blit(text_surface, ( (WIN_SIZE[0] - text_surface.get_width()) / 2, 10))
+
+        pygame.display.update()
+        clock.tick(FPS)
