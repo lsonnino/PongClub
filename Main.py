307467f0c7c4d2632d1e9e2178908b73874c21b7
lsonnino <lorenzo@sonnino.com>
2019-07-20 10:52:18 +0200

    Trained up to 32

--- Main.py
+++ Main.py
@@ -3,17 +3,18 @@ from train import *
 
 NAME = "PongClub"
 NUMBER_OF_AI = 2  # between 0 and 2
-NUMBER_OF_GEN = 8
+NUMBER_OF_GEN = 16
+FROM_GEN = 16
 
 
 TRAINING = 0
 PLAYING = 1
 AGAINST_BEST = 2
 action = AGAINST_BEST
-play_against = 16
+play_against = 32
 
 if action == TRAINING:
-    train(num_of_games=NUMBER_OF_GEN, from_num=8)
+    train(num_of_games=NUMBER_OF_GEN, from_num=FROM_GEN)
 elif action == PLAYING:
     gui.game(NAME, NUMBER_OF_AI)
 elif action == AGAINST_BEST:
